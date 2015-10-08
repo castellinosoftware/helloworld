defcff3b85a510c6301e44dd82b65b7c68a688b9
Marco <sviluppo@castellino.com>
2015-10-08 17:07:02 +0200

    Added feature1, i.e. new message on console

--- HelloWorld/src/com/castellino/test/HelloWorld.java
+++ HelloWorld/src/com/castellino/test/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld
   public static void main(String[] args)
   {
     System.out.println("Hello World! This is me...");
+    System.out.println("Feature 1 implementation");
   }
 
 }
